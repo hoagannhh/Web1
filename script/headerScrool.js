812cbe506267e9b966bc4f82ccac0eaf3aa72a35
9Thanh <tranchinhthanh09@gmail.com>
2025-10-06 23:10:29 +0700

    test

--- script/headerScrool.js
+++ script/headerScrool.js
@@ -1,14 +1,12 @@
 const header = document.querySelector(".task-bar");
-const sidebar = document.querySelector(".sidebar");
+
 let lastScroll = 0;
 window.addEventListener("scroll", () => {
   let currentScroll = window.pageYOffset;
   if (lastScroll > currentScroll) {
     header.classList.remove("hide");
-    sidebar.classList.add("push");
   } else {
     header.classList.add("hide");
-    sidebar.classList.remove("push");
   }
   lastScroll = currentScroll;
 });
